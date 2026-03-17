2d7dd0e4dc364fa8d7de9bcdb281870b9db58eea
6Spidey22 <brando.warnke@gmail.com>
2026-03-17 10:01:31 -0500

    Implement web crawler for Cornell College site
    Refactored the web scraping logic to implement a web crawler that visits and fetches links from the specified domain.

--- web_scrape.py
+++ web_scrape.py
@@ -1,36 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
-from quote_spider import QuoteSpider
+from urllib.parse import urljoin, urlparse
+
+#Global Variables
+start_url = "https://www.cornellcollege.edu"
+domain = urlparse(start_url).netloc
+visited_urls = set()
+urls_to_visit = [start_url]
 
 
-def html_store(url):
-    #this function takes a url and grabs the html and writes it to the cornell_college.html file
-    html = requests.get(url)
+def web_crawl(url):
+    if url in visited_urls:
+        #If the url has already been visited then no need to search it again
+        return
 
-    if html.status_code == 200:
-        #if html.status_code == 200 that means the request was successful
-        with open('cornell_college.html', 'w', encoding='utf-8') as f:
-            f.write(html.text)
-        print("Successfully stored HTML in cornell_college.html")
+    print(f"Crawling {url}")
+    #Adding current url to visited_urls
+    visited_urls.add(url)
 
+    #gets the HTML from the url
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
     else:
-        raise Exception("Failed to store HTML in cornell_college.html")
+        print(f"Error fetching {url}")
+        return
 
-def fetch_links(soup):
-    #this function finds all links stored in a BeautifulSoup object and stores them in a list
-    links = [link.get('href') for link in soup.find_all('a')]
-    return links
+    #finds all
+    tags = soup.find_all(["h2", "p"])
+
+    #Finds all links in the current url and adds them to the urls_to_visit if it's within the domain
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        if href:
+            full_url = urljoin(url, href)
+            if urlparse(full_url).netloc == domain and full_url not in visited_urls:
+                urls_to_visit.append(full_url)
+
+    #Loops through all links in a FIFO queue format
+    while urls_to_visit:
+        url = urls_to_visit.pop(0)
+        web_crawl(url)
 
 
 if __name__ == '__main__':
-    url = "https://www.cornellcollege.edu/"
-    html_store(url)
-
-    #creating the BeautifulSoup object that will store the html information from cornell_college.html
-    with open('cornell_college.html', 'r', encoding='utf-8') as f:
-        html_doc = f.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    print(f"Page Title: {soup.title.string}\n\n\n")
-    links = fetch_links(soup)
-    new_spider = QuoteSpider()
-    new_spider.set_start_urls(links)
+    url = urls_to_visit.pop(0)
+    web_crawl(url)
