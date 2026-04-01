a81040034d02fdae3c1977d9728382c4a2460d47
6Spidey22 <brando.warnke@gmail.com>
2026-04-01 09:21:16 -0500

    Set up Flask app with home route

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template, request, jsonify, session
+import ollama
 
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def home():
+    return render_template("ask_ulysses.html")
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
