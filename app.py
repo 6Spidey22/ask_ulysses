b15b6acdcbaf3ed58a21924d8c90b120a991095e
6Spidey22 <brando.warnke@gmail.com>
2026-04-03 14:46:47 -0500

    Refactor Flask app to handle user questions
    Refactor home route to index and add POST handling for user questions. Integrate tool_llm_response and process response for rendering.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request, jsonify, session
-import ollama
+from flask import Flask, render_template, request, session
+from ask_ulysses import tool_llm_response
+import markdown
+from bs4 import BeautifulSoup
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route("/")
-def home():
-    return render_template("ask_ulysses.html")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    response_html = None #default
+
+    if request.method == "POST":
+        user_question = request.form.get("question")
+        response = tool_llm_response(user_question)
+        response = response["message"]["content"]
+        print(response)
+        response_html = markdown.markdown(response)
+        soup = BeautifulSoup(response_html, "html.parser")
+        for a in soup.find_all("a"):
+            a["target"] = "_blank"
+            a["rel"] = "noopener noreferrer"
+
+        response_html = str(soup)
+
+    return render_template("ask_ulysses.html", response=response_html)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000, debug = True)
